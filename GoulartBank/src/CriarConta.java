fd46658e49d1915d70e034a7f5aaf4300d1eb3fe
Caio Vitor Goulart <cgoulart.rj@gmail.com>
2021-07-04 03:43:40 -0300

    Iniciando projeto java criando um banco

---
+++ GoulartBank/src/CriarConta.java
@@ -0,0 +1,11 @@
+
+public class CriarConta {
+	public static void main(String[] args) {
+		Conta primeira = new Conta();
+		primeira.saldo = 200;
+		System.out.println(primeira.saldo);
+
+		Conta segundaConta = new Conta();
+		segundaConta.saldo+=300;
+	}
+}
