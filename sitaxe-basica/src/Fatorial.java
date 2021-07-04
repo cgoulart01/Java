5c67d629083493c4b44e847531ba0aa903438d70
Caio Vitor Goulart <cgoulart.rj@gmail.com>
2021-07-03 23:46:08 -0300

    printar na tela o fatorial de 0 a 10

--- sitaxe-basica/src/Fatorial.java
+++ sitaxe-basica/src/Fatorial.java
@@ -2,15 +2,18 @@
 public class Fatorial {
 	public static void main(String[] args) {
 		
-		for (int contador=0;contador<=10;contador++) { 
-			 int fat = fatorial(contador);
-			System.out.println(" o resultado de "+contador+"! é "+fat);
+		for (var contador=0;contador<=10;contador++) { 
+			 var fat = fatorial(contador);
+			 if(contador<=9)
+				 System.out.println(" o resultado de  "+contador+"! é "+fat);
+			 else
+			 System.out.println(" o resultado de "+contador+"! é "+fat);
 		}
 		
 		}
 	public static int fatorial (int numero) {
-		int fatorial = 1;
-		for(int i = 1;i<=numero;i++) {
+		var fatorial = 1;
+		for(var i = 1;i<=numero;i++) {
 			fatorial *=i;
 		}
 		return fatorial;
