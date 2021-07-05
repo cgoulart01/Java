ee8ea6b317250b9db66048321fda76a261dcecb7
Caio Vitor Goulart <cgoulart.rj@gmail.com>
2021-07-05 01:54:13 -0300

    Refatorei o codigo com as novas regras de Cliente

--- GoulartBank-composto/src/TestaBanco.java
+++ GoulartBank-composto/src/TestaBanco.java
@@ -1,10 +1,11 @@
 
 public class TestaBanco {
 	public static void main(String[] args) {
-		Cliente paulo = new Cliente();
-		paulo.nome = "Paulo Silveira";
-		paulo.cpf = "111.111.111-44";
-		paulo.profissao = "Empresário";
+		String nome = "Paulo Silveira";
+		String cpf = "111.111.111-44";
+		String profissao = "Empresário";
+		Cliente paulo = new Cliente(nome,cpf,profissao);
+			
 		Conta contaDoPaulo = new Conta(paulo);
 		contaDoPaulo.deposita(1000);
 		
@@ -12,15 +13,15 @@ public class TestaBanco {
 		contaDoPaulo2.deposita(1200);
 		Conta contaDoPaulo3 = new Conta(paulo);
 		contaDoPaulo3.deposita(1500);
-		System.out.println("Conta 1 do "+ contaDoPaulo.getTitular().nome);
+		System.out.println("Conta 1 do "+ contaDoPaulo.getTitular().getNome());
 		System.out.println(contaDoPaulo.getAgencia());
 		System.out.println(contaDoPaulo.getNumero());
 		System.out.println(contaDoPaulo.getSaldo());
-		System.out.println("Conta 2 do "+ contaDoPaulo2.getTitular().nome);
+		System.out.println("Conta 2 do "+ contaDoPaulo2.getTitular().getNome());
 		System.out.println(contaDoPaulo2.getAgencia());
 		System.out.println(contaDoPaulo2.getNumero());
 		System.out.println(contaDoPaulo2.getSaldo());
-		System.out.println("Conta 3 do " + contaDoPaulo3.getTitular().nome);
+		System.out.println("Conta 3 do " + contaDoPaulo3.getTitular().getNome());
 		System.out.println(contaDoPaulo3.getAgencia());
 		System.out.println(contaDoPaulo3.getNumero());
 		System.out.println(contaDoPaulo3.getSaldo());
